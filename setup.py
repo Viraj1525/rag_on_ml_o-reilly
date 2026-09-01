41d1a1bfae92eea66baf7017103029ca57b5a559
Viraj1525 <2300521520073@ietlucknow.ac.in>
2026-09-02 03:10:45 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ml_bot",
+    version="0.1.0",
+    author_email="virajagrawal.1525@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
